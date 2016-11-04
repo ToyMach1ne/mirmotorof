15723223b8d24daf1b3ab6bb8eefd812a878ee90
Toy Mach1ne <forvinwolves@gmail.com>
2016-11-04 12:46:02 +0200

    articles

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -21,3 +21,17 @@ if (typeof jQuery === 'undefined') {
   console.log('jQuery has loaded');
 }
 // Place any jQuery/helper plugins in here.
+
+
+$(document).ready(function() {
+
+  $("#owl-demo").owlCarousel({
+
+      autoPlay: 3000, //Set AutoPlay to 3 seconds
+      items : 4,
+      itemsDesktop : [1199,3],
+      itemsDesktopSmall : [979,3]
+
+  });
+
+});
