871d01ae7fcc14ee073016211d06279c8b069c87
Toy Mach1ne <forvinwolves@gmail.com>
2016-11-04 14:00:28 +0200

    gallery + contacts

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -35,3 +35,24 @@ $(document).ready(function() {
   });
 
 });
+
+
+$(document).ready(function() {
+
+  $("#owl-demo-gallery").owlCarousel({
+      autoPlay : true,
+      navigation : true, // Show next and prev buttons
+      slideSpeed : 300,
+      paginationSpeed : 400,
+      singleItem:true
+
+      // "singleItem:true" is a shortcut for:
+      // items : 1,
+      // itemsDesktop : false,
+      // itemsDesktopSmall : false,
+      // itemsTablet: false,
+      // itemsMobile : false
+
+  });
+
+});
