124d97302fe8c1f88729b077a5e589301daf3702
crazyyy <crazyyy@gmail.com>
2016-11-15 01:57:15 +0200

    fixes

--- wordpress/wp-content/themes/wp-motor/sidebar.php
+++ wordpress/wp-content/themes/wp-motor/sidebar.php
@@ -1,8 +1,28 @@
-<aside class="sidebar" role="complementary">
+<div class="container">
+  <aside class="sidebar row" role="complementary">
+    <?php if ( is_active_sidebar('widgetarea1') ) : ?>
+      <?php dynamic_sidebar( 'widgetarea1' ); ?>
+    <?php else : ?>
+      <div class="col-md-4">
+        <a class="footer-phone--top" href="tel:+38<?php the_field('pre_phone1' , 4); ?><?php the_field('phone1' , 4); ?>">
+          <span><?php the_field('pre_phone1' , 4); ?></span><?php the_field('phone1' , 4); ?>
+        </a>
+        <a class="footer-phone--bott" href="tel:+38<?php the_field('pre_phone2' , 4); ?><?php the_field('phone2' , 4); ?>">
+          <span><?php the_field('pre_phone2' , 4); ?></span><?php the_field('phone2' , 4); ?>
+        </a>
+      </div>
+      <div class="col-md-4">
+        <p class="footer-adress">Г. Киев, Ул. Автозаводская 50/7</p>
+      </div>
+      <div class="col-md-4 social-block">
+        <ul class="footer-social">
+          <li><a href="#" class="vk"></a></li>
+          <li><a href="#" class="fb"></a></li>
+          <li><a href="#" class="ggl"></a></li>
+          <li><a href="#" class="twit"></a></li>
+        </ul>
+      </div><!-- col-md-4 social-block -->
+    <?php endif; ?>
+  </aside><!-- /sidebar -->
+</div>
 
-  <?php if ( is_active_sidebar('widgetarea1') ) : ?>
-    <?php dynamic_sidebar( 'widgetarea1' ); ?>
-  <?php else : ?>
-
-  <?php endif; ?>
-</aside><!-- /sidebar -->
--- wordpress/wp-content/themes/wp-motor/single.php
+++ wordpress/wp-content/themes/wp-motor/single.php
@@ -16,25 +16,25 @@
         <div class="row">
           <div class="col-md-12">
             <div id="owl-demo">
-            <?php query_posts("showposts=100&cat=3"); ?>
-            <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-              <div class="item">
-              <?php if ( has_post_thumbnail()) :?>
-                 <a class="single-thumb" href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
-                   <?php the_post_thumbnail('normal'); ?>
-                 </a>
-              <?php endif; ?><!-- /post thumbnail -->
+              <?php query_posts("showposts=100&cat=3"); ?>
+              <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+                <div class="item">
+                <?php if ( has_post_thumbnail()) :?>
+                   <a class="single-thumb" href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
+                     <?php the_post_thumbnail('normal'); ?>
+                   </a>
+                <?php endif; ?><!-- /post thumbnail -->
                 <h6><?php the_title(); ?></h6>
                 <p class="article-descr"><?php the_excerpt(); ?></p>
                 <p class="article-date"><?php the_time('j F Y'); ?><a href="<?php the_permalink(); ?>"><span>подробнее</span></a></p>
               </div>
-              <?php endwhile; endif; ?>
-              <?php wp_reset_query(); ?>
-            </div>
+            <?php endwhile; endif; ?>
+            <?php wp_reset_query(); ?>
           </div>
-        </div><!-- row -->
-      </div><!-- container -->
-    </div><!-- article-slider -->
+        </div>
+      </div><!-- row -->
+    </div><!-- container -->
+  </div><!-- article-slider -->
 
   </section><!-- /section -->
 </div><!-- /wrapper -->
