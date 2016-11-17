2faf122202b6ca686d82248c701cfb7f08909826
Toy Mach1ne <forvinwolves@gmail.com>
2016-11-17 13:42:02 +0200

    fixes vol.1

--- wordpress/wp-content/themes/wp-motor/footer.php
+++ wordpress/wp-content/themes/wp-motor/footer.php
@@ -8,9 +8,9 @@
   </script>
 <script>
       function initMap() {
-        var uluru = {lat: 50.516975, lng: 30.455216 };
+        var uluru = {lat: 50.5180698, lng: 30.4570509 };
         var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 13,
+          zoom: 15,
           center: uluru
         });
         var marker = new google.maps.Marker({
