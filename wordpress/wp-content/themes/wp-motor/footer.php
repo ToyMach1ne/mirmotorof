e292a66b0ae3b095ad265f7204b91bf2c32cbdc3
crazyyy <crazyyy@gmail.com>
2016-12-05 18:02:53 +0200

    before new markes

--- wordpress/wp-content/themes/wp-motor/footer.php
+++ wordpress/wp-content/themes/wp-motor/footer.php
@@ -1,31 +1,26 @@
   <footer role="contentinfo">
-<div id="map" class="goole-map"></div>
-
-<?php if ( is_page('10') ){ ?>
-    <script>
-      function initMap() {
-        var uluru = {lat: 50.5180698, lng: 30.4570509};
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 10,
-          center: uluru
-        });
-        var marker = new google.maps.Marker({
-          position: uluru,
-          map: map
-        });
-      }
-    </script>
-<script src="//maps.googleapis.com/maps/api/js?key=AIzaSyDWzvd7YhjsMCOVT7sji6gNhK3th5lqJh8&callback=initMap" async="" defer="defer" type="text/javascript">
-</script>
-
-    <?php } else { ?>
-            <img src="<?php echo get_template_directory_uri(); ?>/img/map.png" alt="<?php wp_title( '' ); ?>" title="<?php wp_title( '' ); ?>" class="map-img">
-              <?php  } ?>
-          <?php get_sidebar(); ?>
+    <div id="map" class="goole-map"></div>
+      <?php if ( is_page('10') ){ ?>
+        <script>
+          function initMap() {
+            var uluru = {lat: 50.5180698, lng: 30.4570509};
+            var map = new google.maps.Map(document.getElementById('map'), {
+              zoom: 10,
+              center: uluru
+            });
+            var marker = new google.maps.Marker({
+              position: uluru,
+              map: map
+            });
+          }
+        </script>
+        <script src="//maps.googleapis.com/maps/api/js?key=AIzaSyDWzvd7YhjsMCOVT7sji6gNhK3th5lqJh8&callback=initMap" async="" defer="defer" type="text/javascript"></script>
+      <?php } else { ?>
+        <img src="<?php echo get_template_directory_uri(); ?>/img/map.png" alt="<?php wp_title( '' ); ?>" title="<?php wp_title( '' ); ?>" class="map-img">
+      <?php  } ?>
+    <?php get_sidebar(); ?>
   </footer><!-- /footer -->
-
   <?php wp_footer(); ?>
-
 </body>
 </html>
 
--- wordpress/wp-content/themes/wp-motor/header.php
+++ wordpress/wp-content/themes/wp-motor/header.php
@@ -4,9 +4,8 @@
 
   <meta charset="UTF-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<!--   <meta name="viewport" content="width=device-width, initial-scale=1.0"> -->
   <title><?php wp_title( '' ); ?><?php if ( wp_title( '', false ) ) { echo ' :'; } ?> <?php bloginfo( 'name' ); ?></title>
-
 
   <link href="<?php echo get_template_directory_uri(); ?>/favicon.ico" rel="shortcut icon">
 
@@ -19,26 +18,26 @@
 
   <?php wp_head(); ?>
 
-	<!-- Facebook Pixel Code -->
-	<script>
-	!function(f,b,e,v,n,t,s)
-	{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-	n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-	if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-	n.queue=[];t=b.createElement(e);t.async=!0;
-	t.src=v;s=b.getElementsByTagName(e)[0];
-	s.parentNode.insertBefore(t,s)}(window,document,'script',
-	'https://connect.facebook.net/en_US/fbevents.js');
+  <!-- Facebook Pixel Code -->
+  <script>
+  !function(f,b,e,v,n,t,s)
+  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+  n.queue=[];t=b.createElement(e);t.async=!0;
+  t.src=v;s=b.getElementsByTagName(e)[0];
+  s.parentNode.insertBefore(t,s)}(window,document,'script',
+  'https://connect.facebook.net/en_US/fbevents.js');
 
-	fbq('init', '1072183672865696');
-	fbq('track', 'PageView');
-	</script>
-	<noscript>
-	<img height="1" width="1"
-	src="https://www.facebook.com/tr?id=1072183672865696&ev=PageView
-	&noscript=1"/>
-	</noscript>
-	<!-- End Facebook Pixel Code -->
+  fbq('init', '1072183672865696');
+  fbq('track', 'PageView');
+  </script>
+  <noscript>
+  <img height="1" width="1"
+  src="https://www.facebook.com/tr?id=1072183672865696&ev=PageView
+  &noscript=1"/>
+  </noscript>
+  <!-- End Facebook Pixel Code -->
 </head>
 <body <?php body_class(); ?>>
 <!-- wrapper -->
