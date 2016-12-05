3b3b2b941ad682a6b1b6065e36f2ca58d92dac55
Toy Mach1ne <forvinwolves@gmail.com>
2016-12-05 15:43:25 +0200

    mobile

--- wordpress/wp-content/themes/wp-motor/header.php
+++ wordpress/wp-content/themes/wp-motor/header.php
@@ -4,7 +4,7 @@
 
   <meta charset="UTF-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <meta name="viewport" width=1200, initial-scale="1">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title><?php wp_title( '' ); ?><?php if ( wp_title( '', false ) ) { echo ' :'; } ?> <?php bloginfo( 'name' ); ?></title>
 
 
